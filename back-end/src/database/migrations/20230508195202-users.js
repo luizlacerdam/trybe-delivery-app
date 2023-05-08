f76d595b4e333cc3ecca4c988b89d747296a9279
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-05-08 17:51:21 -0300

    :sparkles: [feat] users-migration

---
+++ back-end/src/database/migrations/20230508195202-users.js
@@ -0,0 +1,24 @@
+/**
+* @param {import('sequelize').QueryInterface} queryInterface
+* @param {import('sequelize').Sequelize } Sequelize
+*/
+module.exports = {
+  async up(queryInterface, Sequelize) {
+      await queryInterface.createTable('users', { 
+        id: {
+          primaryKey: true,
+          allowNull: false,
+          autoIncrement: true,
+          type: Sequelize.INTEGER,
+        },
+        name: { allowNull: false, type: Sequelize.STRING },
+        email: { allowNull: false, type: Sequelize.STRING },
+        password: { allowNull: false, type: Sequelize.STRING },
+        role: { allowNull: false, type: Sequelize.STRING },
+       });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('users');
+  },
+};
