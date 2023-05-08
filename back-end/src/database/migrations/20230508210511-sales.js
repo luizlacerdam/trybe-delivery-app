2becbf3e689b07d8ee3a298eeb4952651e976570
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-05-08 18:19:38 -0300

    :sparkles: [feat] sales-migration

---
+++ back-end/src/database/migrations/20230508210511-sales.js
@@ -0,0 +1,24 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('sales', { 
+      id: {
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+        type: Sequelize.INTEGER,
+      },
+      userId: { allowNull: false, type: Sequelize.INTEGER, field: 'user_id' },
+      sellerId: { allowNull: false, type: Sequelize.INTEGER, field: 'seller_id' },
+      totalPrice: { allowNull: false, type: Sequelize.DECIMAL(9, 2), field: 'total_price' },
+      deliveryAdress: { allowNull: false, type: Sequelize.STRING, field: 'delivery_adress' },
+      deliveryNumber: { allowNull: false, type: Sequelize.STRING, field: 'delivery_number' },
+      saleDate: { allowNull: false, type: Sequelize.DATE, field: 'sale_date' },
+      status: { allowNull: false, type: Sequelize.STRING },
+
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('sales');
+  },
+};
