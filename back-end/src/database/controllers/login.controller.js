86d5c45a0acf92e5ae9a69641d9b9c5c89ef545d
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-05-09 20:11:40 -0300

    :sparkles: [feat] login-controller

---
+++ back-end/src/database/controllers/login.controller.js
@@ -0,0 +1,29 @@
+require('dotenv/config');
+
+const isBodyValid = (email, password) => email && password;
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!isBodyValid(email, password)) {
+      return res.status(400).json({ message: 'Some required fields are missing' });
+    }
+  
+    const user = await UserService.getByEmail(email);
+  
+    if (!user || user.password !== password) {
+      return res.status(400).json({ message: 'Invalid fields' }); 
+    }
+
+    const token = tokenGenerator({ data: { userId: user.id } });
+
+    res.status(200).json({ token });
+  } catch (err) {
+    return res.status(500).json({ message: 'Erro interno', error: err.message });
+  }
+};
+
+module.exports = {
+    login,
+};
