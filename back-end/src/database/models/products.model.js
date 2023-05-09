868d003afa679c135197bd2b82001bceb13bfcb7
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-05-08 21:52:57 -0300

    :sparkles: [feat] products-model

---
+++ back-end/src/database/models/products.model.js
@@ -0,0 +1,20 @@
+/**
+* @param {import('sequelize').DataTypes} DataTypes
+* @param {import('sequelize').Sequelize } Sequelize
+* @returns
+*/
+
+module.exports = (sequelize, DataTypes) => {
+    const Products = sequelize.define(
+  'products', 
+    {
+        id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+        name: { allowNull: false, type: DataTypes.STRING },
+        price: { allowNull: false, type: DataTypes.DECIMAL(4, 2) },
+        urlImage: { allowNull: false, type: DataTypes.STRING },
+    },
+  { timestamps: false, tableName: 'products', underscored: true },
+    );
+    
+    return Products;
+  };
