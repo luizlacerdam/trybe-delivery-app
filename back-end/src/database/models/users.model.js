cd7594070d31160aebc1689e0b3cefff3d5a7cca
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-05-08 22:02:18 -0300

    :recycle: [refa] user-model-associate-sales

--- back-end/src/database/models/users.model.js
+++ back-end/src/database/models/users.model.js
@@ -18,7 +18,7 @@ module.exports = (sequelize, DataTypes) => {
   );
   User.associate = (models) => {
     User.hasMany(models.Sale, { foreignKey: 'userId', as: 'user' });
-    User.hasMany(models.Sale, { foreignKey: 'userId', as: 'user' });
+    User.hasMany(models.Sale, { foreignKey: 'sellerId', as: 'seller' });
 };
   return User;
 };
