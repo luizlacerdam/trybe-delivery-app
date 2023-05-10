e02100ba158ce540ff218dc2d4e7a983b7369252
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-05-10 16:49:22 -0300

    :sparkles: [feat] Home-Page-with-redirect-to-login

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
+import Home from './pages/Home';
 
 function App() {
   return (
     <Switch>
-      <Route exact path="/" component={ Login } />
+      <Route exact path="/" component={ Home } />
       <Route exact path="/login" component={ Login } />
     </Switch>
   );
