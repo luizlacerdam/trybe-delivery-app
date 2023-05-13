dd5135f14c5120f2170d7b4ed1b2c3f03237c797
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-05-12 23:21:43 -0300

    :sparkles: [feat] create-service

--- back-end/src/services/user.service.js
+++ back-end/src/services/user.service.js
@@ -1,7 +1,10 @@
 const { User } = require('../database/models');
 
+const create = (newUser) => User.create(newUser);
+
 const getByEmail = (email) => User.findOne({ where: { email } });
 
 module.exports = {
     getByEmail,
+    create,
 };
