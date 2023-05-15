1bf68b3d0cab1845fa553b4bf820d345c1758b10
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-05-15 10:09:28 -0300

    :sparkles: [feat] product-service-getAll-getById

--- back-end/src/services/product.service.js
+++ back-end/src/services/product.service.js
@@ -1,3 +1,13 @@
 const { Product } = require('../database/models');
 
 const create = async (newProduct) => Product.create(newProduct);
+
+const getAll = async () => Product.getAll();
+
+const getById = async (id) => Product.getById(id);
+
+module.exports = {
+    create,
+    getAll,
+    getById,
+};
