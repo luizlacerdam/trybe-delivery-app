e04e804367da81008aeed57b3a316dba76f859ca
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-05-16 15:30:16 -0300

    :sparkles: [feat] Loading-component

--- front-end/src/pages/Customer/Products/index.jsx
+++ front-end/src/pages/Customer/Products/index.jsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from '../components/Navbar';
 import Card from './components/Card';
+import { requestData } from '../../../services/requests';
 
 export default function ProductPage() {
+  const [products, setProducts] = useState([{}]);
+
+  useEffect(() => {
+    const getProductsList = async () => {
+      const data = await requestData('/customer/products');
+      setProducts(data);
+    };
+    getProductsList();
+  }, []);
   return (
     <body>
-      <Navbar />
-      <Card />
+      {/* <Navbar /> */}
+      <section className="cards">
+        {products.map(({ id, role, imgUrl, price, title }, index) => (
+          <Card
+            id={ id }
+            role={ role }
+            imgUrl={ imgUrl }
+            price={ price }
+            title={ title }
+            key={ index }
+          />
+        ))}
+      </section>
     </body>
   );
 }
