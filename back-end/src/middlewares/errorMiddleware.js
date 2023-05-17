8bcf6d8ecfd82df7583e159f209f0ed5e933bfd4
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-05-17 16:16:28 -0300

    :bug: [fix] fixing-evalo-bug

--- back-end/src/middlewares/errorMiddleware.js
+++ back-end/src/middlewares/errorMiddleware.js
@@ -2,6 +2,7 @@ const errorMiddleware = (err, _req, res, _next) => {
   if (err.statusCode) {
     return res.status(err.statusCode).json({ message: err.message });
   }
+  console.log(err);
   return res.status(500).json({ error: err.message });
 };
 
