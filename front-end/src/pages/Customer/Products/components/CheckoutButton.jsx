6af02f9f8c24c2e19a5acacbbc6716d7d974a09f
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-05-19 20:08:45 -0300

    :sparkles: [feat] req-16

--- front-end/src/pages/Customer/Products/components/CheckoutButton.jsx
+++ front-end/src/pages/Customer/Products/components/CheckoutButton.jsx
@@ -1,20 +1,23 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import { getLocalStorage } from '../../../../utils/localStorageHandling';
 
 export default function CheckoutButton({ total, setTotal }) {
+  const history = useHistory();
   return (
     <button
       data-testid="customer_products__button-cart"
       type="button"
+      onClick={ () => { history.push('/customer/checkout'); } }
+      disabled={ total === 0 }
     >
-      Ver Carrinho: R$
-      {' '}
       <div
         data-testid="customer_products__checkout-bottom-value"
       >
         {total.toFixed(2).replace('.', ',')}
       </div>
+
     </button>
   );
 }
