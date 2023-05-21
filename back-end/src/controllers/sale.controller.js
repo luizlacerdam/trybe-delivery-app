7b00e0d4fd75df922cc4c153c07ba44975c328f1
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-05-21 19:20:09 -0300

    :sparkles: [feat] sale-controller

---
+++ back-end/src/controllers/sale.controller.js
@@ -0,0 +1,36 @@
+const { SaleService } = require('../services');
+
+const create = async (req, res, next) => {
+    const newSale = req.body;
+    try {
+        const data = await SaleService.create(newSale);
+        return res.status(201).json({ data });
+    } catch (error) {
+        next(error);
+    }
+};
+
+const findAll = async (req, res, next) => {
+    try {
+        const data = await SaleService.findAll();
+        return res.status(200).json({ data });
+    } catch (error) {
+        next(error);
+    }
+};
+
+const findByPk = async (req, res, next) => {
+    const { id } = req.params;
+    try {
+        const data = await SaleService.findByPk(id);
+        return res.status(200).json({ data });
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports = {
+    create,
+    findAll,
+    findByPk,
+};
--- back-end/src/services/index.js
+++ back-end/src/services/index.js
@@ -1,7 +1,9 @@
 const UserService = require('./user.service');
 const ProductService = require('./product.service');
+const SaleService = require('./sale.service');
 
 module.exports = {
     UserService,
     ProductService,
+    SaleService,
 };
