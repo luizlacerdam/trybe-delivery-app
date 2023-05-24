00b96d1aeecea5fbe50e2e09e9591045d3734b83
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-05-24 18:14:41 -0300

    :sparkles: [feat] updateStatus-router

--- back-end/src/routers/sale.router.js
+++ back-end/src/routers/sale.router.js
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post('/orders', validateToken, SaleController.create);
 router.get('/orders', validateToken, SaleController.findAll);
 router.get('/orders/:id', validateToken, SaleController.findByPk);
+router.patch('/orders/:id', validateToken, SaleController.updateStatus);
 
 module.exports = router;
