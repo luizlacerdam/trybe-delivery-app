9a9a51d48fa88b7604a90ff6fa9899e2227f3703
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-05-24 10:32:44 -0300

    :sparkles: [feat] OrderItens

---
+++ front-end/src/pages/Customer/OrderDetails/components/OrderItens.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function OrderItens({ index, name, qty, price }) {
+  const subTotal = qty * price;
+  return (
+    <div>
+      <div
+        data-testid={ `customer_order_details__element-order-table-item-number-${index}` }
+      >
+        {index}
+      </div>
+      <div
+        data-testid={ `customer_order_details__element-order-table-name-${index}` }
+      >
+        {name}
+      </div>
+      <div
+        data-testid={ `customer_order_details__element-order-table-quantity-${index}` }
+      >
+        {qty}
+      </div>
+      <div
+        data-testid={ `customer_order_details__element-order-table-unit-price-${index}` }
+      >
+        {price}
+      </div>
+      <div
+        data-testid={ `customer_order_details__element-order-table-sub-total-${index}` }
+      >
+        {subTotal}
+      </div>
+    </div>
+  );
+}
+
+OrderItens.propTypes = ({
+  id: PropTypes.any,
+  name: PropTypes.any,
+  qty: PropTypes.any,
+  price: PropTypes.any,
+}).isRequired;
