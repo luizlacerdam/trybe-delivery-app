4afaea2fb2ce2dafcdb57d19bfb3d94ef678a47f
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-05-27 12:04:38 -0300

    :sparkles: [feat] AddUser-component

--- front-end/src/pages/Users/Admin/components/AddUser.jsx
+++ front-end/src/pages/Users/Admin/components/AddUser.jsx
@@ -1,9 +1,68 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { requestPost } from '../../../../services/requests';
 
 export default function AddUser() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [role, setRole] = useState('');
+  const [isDisabled, setIsDisabled] = useState(true);
+  const [res, setRes] = useState('');
+
+  useEffect(() => {
+    const validateEmail = () => {
+      const regexEmail = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+      return regexEmail.test(email);
+    };
+
+    const validateName = () => {
+      const minCharacters = 12;
+      return name.length >= minCharacters;
+    };
+
+    const validatePassword = () => {
+      const minCharacters = 6;
+      return password.length >= minCharacters;
+    };
+
+    const validateRole = () => {
+      const minCharacters = 1;
+      return role.length >= minCharacters;
+    };
+
+    if (validateEmail() && validatePassword() && validateName() && validateRole()) {
+      setIsDisabled(false);
+    } else {
+      setIsDisabled(true);
+    }
+  }, [email, password, name, role]);
+
+  const createUser = async () => {
+    try {
+      const response = await requestPost('/register', {
+        name,
+        email,
+        password,
+        role,
+      });
+      return response;
+    } catch (error) {
+      console.log(error);
+      setRes(error.response.data.message);
+    }
+  };
+
+  const handleClick = async () => {
+    const response = await createUser();
+    if (response) {
+      setRes('Usuário cadastrado com sucesso!');
+    }
+  };
+
   return (
     <div>
       <span>Cadastrar novo usuário</span>
+      <p>{res}</p>
       <form>
         <label htmlFor="new-user-name">
           Nome
@@ -11,6 +70,8 @@ export default function AddUser() {
             data-testid="admin_manage__input-name"
             id="new-user-name"
             type="text"
+            value={ name }
+            onChange={ ({ target }) => setName(target.value) }
           />
         </label>
         <label htmlFor="new-user-email">
@@ -19,6 +80,8 @@ export default function AddUser() {
             data-testid="admin_manage__input-email"
             id="new-user-email"
             type="email"
+            value={ email }
+            onChange={ ({ target }) => setEmail(target.value) }
           />
         </label>
         <label htmlFor="new-user-password">
@@ -27,6 +90,8 @@ export default function AddUser() {
             data-testid="admin_manage__input-password"
             id="new-user-password"
             type="password"
+            value={ password }
+            onChange={ ({ target }) => setPassword(target.value) }
           />
         </label>
         <label htmlFor="new-user-select">
@@ -35,16 +100,21 @@ export default function AddUser() {
             data-testid="admin_manage__select-role"
             name=""
             id="new-user-select"
+            value={ role }
+            onChange={ ({ target }) => setRole(target.value) }
           >
             <option>Escolha o tipo</option>
-            <option>Vendedor</option>
-            <option>Cliente</option>
-            <option>Administrador</option>
+            <option value="seller">Vendedor</option>
+            <option value="customer">Cliente</option>
+            <option value="administrator">Administrador</option>
           </select>
         </label>
         <button
           data-testid="admin_manage__button-register"
           type="button"
+          onClick={ handleClick }
+          disabled={ isDisabled }
+
         >
           CADASTRAR
         </button>
