2abf6f15f4e573810d324e34500fbae836abb31b
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-08-20 10:23:16 -0300

    :sparkles: [feat] added-middleware-to-routers

--- back-end/src/routers/product.router.js
+++ back-end/src/routers/product.router.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const { ProductController } = require('../controllers');
+const { validateToken } = require('../middlewares/validateToken');
+const { isAdmin } = require('../middlewares/isAdmin');
 
 const router = express.Router();
 
-router.post('/', ProductController.create);
-router.get('/', ProductController.findAll);
-router.get('/:id', ProductController.findByPk);
+router.post('/', validateToken, isAdmin, ProductController.create);
+router.get('/', validateToken, isAdmin, ProductController.findAll);
+router.get('/:id', validateToken, isAdmin, ProductController.findByPk);
 
 module.exports = router;
--- back-end/src/routers/user.router.js
+++ back-end/src/routers/user.router.js
@@ -5,7 +5,7 @@ const { validateToken } = require('../middlewares/validateToken');
 const router = express.Router();
 
 router.get('/', validateToken, UserController.userValidation);
-router.post('/products', ProductController.create);
+router.post('/products', validateToken, ProductController.create);
 router.get('/products', validateToken, ProductController.findAll);
-router.get('/products/:id', ProductController.findByPk);
+router.get('/products/:id', validateToken, ProductController.findByPk);
 module.exports = router;
